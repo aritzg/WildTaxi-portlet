edb3dd6ef0c377f12be4b0d24745cf0fd539494e
Aritz Galdos <galdos.aritz@gmail.com>
2011-06-06 21:25:20 +0200

    test

---
+++ docroot/WEB-INF/src/net/sareweb/wildtaxi/p000/ViewRequestsPortlet.java
@@ -0,0 +1,19 @@
+package net.sareweb.wildtaxi.p000;
+
+import java.io.IOException;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+public class ViewRequestsPortlet extends MVCPortlet {
+
+	@Override
+	public void doView(RenderRequest renderRequest,
+			RenderResponse renderResponse) throws IOException, PortletException {
+		// TODO Auto-generated method stub
+		super.doView(renderRequest, renderResponse);
+	}
+}
